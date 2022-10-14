22652fbcb5a816ee867398896779379d64132dbf
h-nakamori <115455235+h-nakamori@users.noreply.github.com>
2022-10-14 12:45:53 +0900

    初回コミット HelloWorld

---
+++ components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+
+}
+</script>
+
+<style lang="" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <hello-world />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component
+export default class extends Vue {
+
+}
+</script>
+
+<style lang="" scoped>
+</style>
